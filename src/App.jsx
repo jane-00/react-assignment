d6a0670e6438ada6adfdae5aca6a2b1d3482ca4e
joanna <joanna.kalisz03@gmail.com>
2023-03-12 00:58:00 +0100

    cleaned up the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,35 @@
 import { useState } from "react";
-//import { SketchPicker } from "react-color";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Slider from "./components/Slider";
 import ColorPicker from "./components/ColorPicker";
-
 import NumberInput from "./components/NumberInput";
 import Drawing from "./components/Drawing";
 
 function App() {
-  const [radius, setRadius] = useState(15);
+
   const [favNum, setfavNum] = useState(1);
   const [dur, setDur] = useState(10);
   const [colorPicker, setColorPicker] = useState("#19cad7");
 
   return (
     <div className="App">
-
-
       <div className="Main__generator">
-
         <div className="svg__container">
-
-          <Drawing dur={dur} favNum={favNum} radius={radius} color={colorPicker} />
+          <Drawing dur={dur} favNum={favNum} color={colorPicker} />
         </div>
 
         <div className="Controlers">
           <ColorPicker
             label="Color scheme"
             value={colorPicker}
-            onValueChange={(color) => {
-              setColorPicker(color);
-              console.log(colorPicker)
-            }
-            }
+            onValueChange={(color) => setColorPicker(color)}
           />
-
           <NumberInput
             min={0}
-            max={100} // this still lets you enter more than 50
+            max={100}
             label="Your favourite number"
             value={favNum}
-            onValueChange={(n) => setfavNum(n)} // can also just be {setfavNum}
+            onValueChange={(n) => setfavNum(n)}
           />
           <Slider
             min={1}
@@ -50,13 +38,8 @@ function App() {
             value={dur}
             onValueChange={(d) => setDur(d)}
           />
-
-        
-          
         </div>
       </div>
-
-
     </div>
   );
 }
